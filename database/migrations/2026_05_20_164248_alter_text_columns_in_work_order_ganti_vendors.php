2d299a96a86909bc5b13cd51721ab8fdd9160f77
rizal <rizalkrenz4@gmail.com>
2026-05-21 14:51:18 +0700

    update

---
+++ database/migrations/2026_05_20_164248_alter_text_columns_in_work_order_ganti_vendors.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE work_order_ganti_vendors 
+            MODIFY keterangan TEXT NULL");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE work_order_ganti_vendors 
+            MODIFY keterangan VARCHAR(255) NULL");
+    }
+};
